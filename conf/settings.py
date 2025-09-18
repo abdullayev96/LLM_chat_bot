a47cca6cdd8bb17517b529b23843221caa2502d2
Burhon Abdullaev <abdullaev_b@turonbank.uz>
2025-09-18 14:35:32 +0500

    update code add grafana

--- conf/settings.py
+++ conf/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'llm',
 
     "rest_framework",
+    "django_prometheus",
 
     "corsheaders",
     # 'drf_yasg',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django_prometheus.middleware.PrometheusBeforeMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_prometheus.middleware.PrometheusAfterMiddleware",
 ]
 
 ROOT_URLCONF = 'conf.urls'
--- conf/urls.py
+++ conf/urls.py
@@ -6,7 +6,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("llm.urls"))
+    path('api/', include("llm.urls")),
+    path("", include("django_prometheus.urls")),
 ]
 
 
